fcc09b4b516873f978567ecade5360836012dcc4
valcar1980 <vcarapella@gmail.com>
2026-08-22 17:47:36 +0100

    function to create mappings (source, medium) and (medium, target)

--- src/test/java/uk/ac/ox/krr/logmap2/mediating_ontologies/CreateComposedMappings.java
+++ src/test/java/uk/ac/ox/krr/logmap2/mediating_ontologies/CreateComposedMappings.java
@@ -15,15 +15,89 @@ import uk.ac.ox.krr.logmap2.LogMap2_Matcher;
 import uk.ac.ox.krr.logmap2.mappings.objects.MappingObjectStr;
 
 public class CreateComposedMappings{
+	private final Set<MappingObjectStr>  s2mMaps;
+	private final Set<MappingObjectStr>  m2tMaps;
+
+	public CreateComposedMappings() {
+		this.s2mMaps = null;
+		this.m2tMaps = null;
+	}
+	
+	private CreateComposedMappings(Set<MappingObjectStr>  s2mMaps, Set<MappingObjectStr>  m2tMaps ) {
+		this.s2mMaps = s2mMaps;
+		this.m2tMaps = m2tMaps;
+	}
 	
 	
+	public Set<MappingObjectStr> getSource2MediumMappings(){
+		return s2mMaps;
+	}
+	
+	public Set<MappingObjectStr> getMedium2TargetMappings(){
+		return m2tMaps;
+	}
+	
+	
+	/**
+	 * Takes source, target and one mediating ontologies and finds all mappings
+	 * for (source, medium) and (medium, target)
+	 * @param source OWLOntology the source ontology
+	 * @param target OWLOntology the target ontology
+	 * @param medium OWLOntology the mediating ontology
+	 * @return CreateCompoSedMappings new instance of CreateComposedMappings
+	 */
+	public CreateComposedMappings triangulateOntologies(OWLOntology source, OWLOntology target, OWLOntology medium) {
+		
+	   System.out.println("Starting the matching task (source, medium)");
+		LogMap2_Matcher sourceMapMedium= new LogMap2_Matcher(source, medium);
+		Set<MappingObjectStr>  s2mMaps = sourceMapMedium.getLogmap2_Mappings();
+		System.out.println("Completing the matching task (source, medium) " +s2mMaps);
+
+		System.out.println("Starting the matching task ( mo_i, target)");
+		LogMap2_Matcher mediumMapTarget = new LogMap2_Matcher(medium, target);
+		Set<MappingObjectStr>  m2tMaps = mediumMapTarget.getLogmap2_Mappings();
+		System.out.println("Completing the matching task ( medium, target). Mappings count " + m2tMaps.size());
+		
+		return new CreateComposedMappings(s2mMaps, m2tMaps);
+	}
+	
+	public Set<MappingObjectStr>  aggregateComposedMapping(Set<MappingObjectStr> source2mo, Set<MappingObjectStr> mo2target){
+		
+		Set<MappingObjectStr>   composedMappings = new HashSet<MappingObjectStr>();
+		
+		for (MappingObjectStr map_mo1 : source2mo){
+			//TODO why only ClassMappings?
+			if (!map_mo1.isClassMapping())
+				continue;
+			for (MappingObjectStr map_mo2 : mo2target){
+				
+				if (!map_mo2.isClassMapping())
+					continue;
+				
+				if (map_mo1.getIRIStrEnt2().equals(map_mo2.getIRIStrEnt1())){
+		
+					
+					MappingObjectStr mapping = new MappingObjectStr(
+							map_mo1.getIRIStrEnt1(), 
+							map_mo2.getIRIStrEnt2(), 
+							(map_mo1.getConfidence()+map_mo2.getConfidence())/2.0, MappingObjectStr.EQ, MappingObjectStr.CLASSES);
+					
+					composedMappings.add(mapping);
+					//TODO addVote as part of statistics
+				}
+				}					
+		} 
+		
+		
+		return composedMappings;
+	}
 	
 	public static void main(String[] args) {
 		
 		
 		String onto1_iri = "file:/home/valentina/Downloads/anatomy-dataset/human.owl";
 		String onto2_iri = "file:/home/valentina/Downloads/anatomy-dataset/mouse.owl";
-
+		String mo_i_iri = "file:" + "/home/valentina/git-repos/test-data/test_onto_output/test-mediating-store/MIO.owl";
 		// String parentPath = "/home/valentina/git-repos/test-data/test_onto_output/test-mediating-store/";
 		// String filePath = parentPath + "logmap_top10_mediating_ontologies.txt";
 		/*
@@ -43,45 +117,25 @@ public class CreateComposedMappings{
 			OWLOntology onto1 = onto_manager.loadOntology(IRI.create(onto1_iri));
 			System.out.println("Loading the second ontology " + onto2_iri);
 			OWLOntology onto2 = onto_manager.loadOntology(IRI.create(onto2_iri));
-			String mo_i_iri = "file:" + "/home/valentina/git-repos/test-data/test_onto_output/test-mediating-store/MIO.owl";
 			System.out.println("Loading the mediating ontology " + mo_i_iri);
 			OWLOntology mo_i = onto_manager.loadOntology(IRI.create(mo_i_iri));
-			System.out.println("Starting the matching task (onto1, mo_i)");
-			LogMap2_Matcher logmap2_onto1_mo_i = new LogMap2_Matcher(onto1, mo_i);
-			System.out.println("Completing the matching task (onto1, mo_i) " +logmap2_onto1_mo_i);
-			Set<MappingObjectStr>  mo2onto1_maps = logmap2_onto1_mo_i.getLogmap2_Mappings();
-			System.out.println("Completing the matching task ( mo_i, onto2). Mappings count " + mo2onto1_maps.size());
+			// System.out.println("Starting the matching task (onto1, mo_i)");
+			// LogMap2_Matcher logmap2_onto1_mo_i = new LogMap2_Matcher(onto1, mo_i);
+			// System.out.println("Completing the matching task (onto1, mo_i) " +logmap2_onto1_mo_i);
+			// Set<MappingObjectStr>  source2mo_maps = logmap2_onto1_mo_i.getLogmap2_Mappings();
+			// System.out.println("Completing the matching task ( mo_i, onto2). Mappings count " + source2mo_maps.size());
 			
-			System.out.println("Starting the matching task ( mo_i, onto2)");
-			LogMap2_Matcher logmap2_mo_i_onto2 = new LogMap2_Matcher(mo_i, onto2);
-			Set<MappingObjectStr>  mo2onto2_maps = logmap2_mo_i_onto2.getLogmap2_Mappings();
-			System.out.println("Completing the matching task ( mo_i, onto2). Mappings count " + mo2onto2_maps.size());
+			// System.out.println("Starting the matching task ( mo_i, onto2)");
+			// LogMap2_Matcher logmap2_mo_i_onto2 = new LogMap2_Matcher(mo_i, onto2);
+			// Set<MappingObjectStr>  mo2target_maps = logmap2_mo_i_onto2.getLogmap2_Mappings();
+			// System.out.println("Completing the matching task ( mo_i, onto2). Mappings count " + mo2target_maps.size());
 			
-			
-			Set<MappingObjectStr>   composed_mappings = new HashSet<MappingObjectStr>();
-			
-			for (MappingObjectStr map_mo1 : mo2onto1_maps){
-				if (!map_mo1.isClassMapping())
-					continue;
-				for (MappingObjectStr map_mo2 : mo2onto2_maps){
-					
-					if (!map_mo2.isClassMapping())
-						continue;
-					
-					if (map_mo1.getIRIStrEnt2().equals(map_mo2.getIRIStrEnt1())){
-			
-						
-						MappingObjectStr mapping = new MappingObjectStr(
-								map_mo1.getIRIStrEnt1(), 
-								map_mo2.getIRIStrEnt2(), 
-								(map_mo1.getConfidence()+map_mo2.getConfidence())/2.0, MappingObjectStr.EQ, MappingObjectStr.CLASSES);
-						
-						composed_mappings.add(mapping);
-					}
-					}					
-			} 
-			
-			System.out.println("There are " + composed_mappings.size() + " composed mappings");
+			CreateComposedMappings mapComposer = new CreateComposedMappings();
+			mapComposer = mapComposer.triangulateOntologies(onto1, onto2, mo_i);
+			Set<MappingObjectStr> s2m = mapComposer.s2mMaps;
+			Set<MappingObjectStr> m2t = mapComposer.m2tMaps;
+			Set<MappingObjectStr>   composedMappings =  mapComposer.aggregateComposedMapping(s2m, m2t);
+			System.out.println("There are " + composedMappings.size() + " composed mappings");
 			//	for (MappingObjectStr cmap:composed_mappings) {
 			//	
 			//}
