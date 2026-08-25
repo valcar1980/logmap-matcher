18a7f308bd2ec9a87b2c2f18d0bcac9dd0450d0f
valcar1980 <vcarapella@gmail.com>
2026-08-25 16:57:09 +0100

    First step runs and stores mediating ontologies

--- src/test/java/uk/ac/ox/krr/logmap2/mediating_ontologies/README.md
+++ src/test/java/uk/ac/ox/krr/logmap2/mediating_ontologies/README.md
@@ -1,4 +1,8 @@
-## How to use the mediating ontologies package
+# How to use the mediating ontologies package
+
+## Initial setup
+
+Before you run the programs in this package...
 
 ### Get your input data
 Our default example starts with the human and mouse ontology
@@ -6,13 +10,51 @@ You can download them manually at OAEI
 human.owl
 mouse.owl
 
-### Steps
+### Structure your parent folder
 
-1. RunMediatingOntologies
+Please create the following folder structure prior to running the programs. The name of the two sub-directories and the config file matters.
+
+```
+<parent folder>/
+├── store-mediating-ontologies/
+├── store-composed-mappings/
+└── config.json
+```
+
+### Write your config file
+
+The config file is very simple
+
+```
+{
+  "sourceOntologyFullPath": "/home/valentina/git-repos/test-data/test_onto_input/human.owl",
+  "targetOntologyFullPath": "/home/valentina//git-repos/test-data/test_onto_input/mouse.owl"
+}
+
+```
+
+## Steps
+
+### 1. RunMediatingOntologies
 
 This gets the top 10 mediating ontologies for the source and target ontology
-Change the hard-coded paths to match what you have downloaded
-Add arguments to the Run Configuration for the file -Xms500M -Xmx15G -DentityExpansionLimit=10000000 --add-opens=java.base/java.lang=ALL-UNNAMED
+To run from command line type in terminal (provided you have built two separate targets for logmap and its tests) with the correct full path to the folder you have created earlier.
 
-2. CreateComposedMappings
-(Same setup as before)
+```
+java -Xms500M -Xmx25G -DentityExpansionLimit=10000000 --add-opens=java.base/java.lang=ALL-UNNAMED
+ *     -cp logmap-matcher-4.0-tests.jar:logmap-matcher-4.0.jar 
+ *         uk.ac.ox.krr.logmap2.mediating_ontologies.RunMediatingOntologiesPipeline <parent folder>
+
+```
+
+### 2. CreateComposedMappings
+
+This is a memory-heavy program, we highly recommend to run from terminal. Do keep the system monitor open while you run to check that memory is not filling up. 
+
+
+
+## TODO
+
+* Make naming and structure of directory for storing less constricting.
+* Expand on config file structure and content
+* (About CreateComposedMappings) The code should be written so that it can be resumed when run a second time, so you don't get to redo everything.
--- src/test/java/uk/ac/ox/krr/logmap2/mediating_ontologies/RunMediatingOntologiesPipeline.java
+++ src/test/java/uk/ac/ox/krr/logmap2/mediating_ontologies/RunMediatingOntologiesPipeline.java
@@ -75,6 +75,7 @@ public class RunMediatingOntologiesPipeline {
 	}
 	}
 	
+	
 	public static void main(String[] args) {
 
 		/*
@@ -87,73 +88,61 @@ public class RunMediatingOntologiesPipeline {
 		moRunner.getParentFolder(args);
 		moRunner.readConfigJSON();
 		
-//		try {
-//			String parentPath = args[0];
-//			File f = new File(parentPath);
-//			if (f.exists() && f.isDirectory()) {
-//				System.out.println("Parent folder exists at " + parentPath);
-//			} else {
-//				System.out.println("Parent folder doesn't exist at " + parentPath);
-//				return;
-//			}
-//		} catch (Exception e) {
-//			System.out.println("Parent folder not provided in the args");
-//			e.printStackTrace();
-//		}
-//		String onto1_iri = "file:/home/valentina/git-repos/test-data/test_onto_input/human.owl";
-//		String onto2_iri = "file:/home/valentina//git-repos/test-data/test_onto_input/mouse.owl";
+
+
+		String onto1_iri = "file:" + moRunner.sourceOntoPath;
+		String onto2_iri = "file:" + moRunner.targetOntoPath;
+
+		String storeOntoPath = moRunner.parentPath + "/store-mediating-ontologies/";
+		String filePath = moRunner.parentPath + "logmap_top10_mediating_ontologies.txt";
 //
-//		String parentPath = "/home/valentina/git-repos/test-data/test_onto_output/test-mediating-store/";
-//		String filePath = parentPath + "logmap_top10_mediating_ontologies.txt";
-//
-//		System.out.println("Starting Mediating Ontologies Pipeline");
-//		CreateMappingsBetweenTwoOntologies onto_mapper = new CreateMappingsBetweenTwoOntologies();
-//		LogMap2_Matcher onto_mappings = onto_mapper.createMappings(onto1_iri, onto2_iri);
-//
+		System.out.println("Starting Mediating Ontologies Pipeline");
+		CreateMappingsBetweenTwoOntologies onto_mapper = new CreateMappingsBetweenTwoOntologies();
+		LogMap2_Matcher onto_mappings = onto_mapper.createMappings(onto1_iri, onto2_iri);
+
 //		/*
 //		 * Identify suitable mediating ontologies and store their label onto a list
 //		 */
-//		FetchMediatingOntologies mo_fetcher = new FetchMediatingOntologies();
-//		List<String> moList = mo_fetcher.extractMediatingOntologyList(onto_mappings);
-//		mo_fetcher.saveListMediatingOntolgies(true, moList, filePath);
-//
-//		/*
-//		 * Store all ontologies from list of mediating ontologies
-//		 */
-//		StoreMediatingOntologies moStorer = new StoreMediatingOntologies();
-//		int countOnto = moList.size();
-//		System.out.println("There are" + countOnto + "mediating ontologies in the list");
-//		
-//		try {
-//			int counter = 1;
-//			for (String ontoStr : moList) {
-//
-//				System.out.println("Fetching ontology No.  " + counter + " label:  " + ontoStr);
-//
-//				// TODO if file exists, skip
-//
-//				boolean isOntoThere = moStorer.checkOntoPath(ontoStr, parentPath);
-//
-//				if (isOntoThere == true) {
-//
-//					System.out.println("Ontology file already exists at location, skipping");
-//					continue;
-//				}
-//				OWLOntology moDownload = moStorer.CallBioPortal(ontoStr, parentPath);
-//				System.out.println("Fetched ontology" + ontoStr);
-//
-//				// TODO store the ontology
-//				moStorer.saveOntology(ontoStr, moDownload, parentPath);
-//				System.out.println("Stored ontology" + ontoStr);
-//
-//				counter += 1;
-//			}
-//		}
-//		catch (Exception e) 
-//		{
-//			e.printStackTrace();
-//		}
-//		System.out.println("All Stored");
+		FetchMediatingOntologies mo_fetcher = new FetchMediatingOntologies();
+		List<String> moList = mo_fetcher.extractMediatingOntologyList(onto_mappings);
+		mo_fetcher.saveListMediatingOntolgies(true, moList, filePath);
+
+		/*
+		 * Store all ontologies from list of mediating ontologies
+		 */
+		StoreMediatingOntologies moStorer = new StoreMediatingOntologies();
+		int countOnto = moList.size();
+		System.out.println("There are" + countOnto + "mediating ontologies in the list");
+		
+		try {
+			int counter = 1;
+			for (String ontoStr : moList) {
+
+				System.out.println("Fetching ontology No.  " + counter + " label:  " + ontoStr);
+
+
+				boolean isOntoThere = moStorer.checkOntoPath(ontoStr, storeOntoPath);
+
+				if (isOntoThere == true) {
+
+					System.out.println("Ontology file already exists at location, skipping");
+					continue;
+				}
+				OWLOntology moDownload = moStorer.CallBioPortal(ontoStr, storeOntoPath);
+				System.out.println("Fetched ontology " + ontoStr);
+
+				
+				moStorer.saveOntology(ontoStr, moDownload, storeOntoPath);
+				System.out.println("Stored ontology " + ontoStr);
+
+				counter += 1;
+			}
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		System.out.println("All Ontologies are stored in " + storeOntoPath);
 	}
 }
 		
