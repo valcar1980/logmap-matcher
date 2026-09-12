8c327717b15f4007e28b8e77d4f2340f56b7a57c
valcar1980 <vcarapella@gmail.com>
2026-09-12 22:34:08 +0100

    Stats now save to csv, but missing csv header

--- src/test/java/uk/ac/ox/krr/logmap2/mediating_ontologies/ProcessComposedMappings.java
+++ src/test/java/uk/ac/ox/krr/logmap2/mediating_ontologies/ProcessComposedMappings.java
@@ -2,6 +2,11 @@ package uk.ac.ox.krr.logmap2.mediating_ontologies;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -13,8 +18,22 @@ import uk.ac.ox.krr.logmap2.mappings.objects.MappingObjectStr;
 import uk.ac.ox.krr.logmap2.oaei.reader.FlatAlignmentReader;
 
 public class ProcessComposedMappings{
+	public List<String> availableStatistics = new ArrayList<String>();
 	
 	public ProcessComposedMappings(){}
+	public ProcessComposedMappings(Boolean saveToCSV) {
+		if(saveToCSV == true) {
+			this.availableStatistics.add("ontology_label");
+			this.availableStatistics.add("all_composed_mappings_count");
+			this.availableStatistics.add("unique_mappings_count_minus_logmap");
+			this.availableStatistics.add("unique_mappings_count_minus_logmap_llm");
+
+		}
+		else {
+			System.out.println("You chose not to save stats to file.");
+		}
+		
+	}
 	
 	
 	/**
@@ -73,10 +92,37 @@ public class ProcessComposedMappings{
 		return readersArray;
 	}
 	
+	public HashMap<String,String> createDataRow(){
+		HashMap<String,String> dataRow = new HashMap<String,String>();
+	
+	for (String item: this.availableStatistics) {
+		dataRow.put(item, null);
+	
+	}
+	return dataRow;
+	}
+	
+	
+	public void addRowToCSV(Writer writer, String eol, HashMap<String,String> dataRow) {
+		//TODO check that the items in the dataRow match the class list of available Stats
+	try {
+		  for (String item : this.availableStatistics) {
+		    writer.append(dataRow.get(item))
+		          .append(',');
+		          //.append(eol);
+		  }
+		  writer.append(eol);
+		} catch (IOException ex) {
+		  ex.printStackTrace(System.err);
+		}
+	}
+	
+
 	public static void main(String[] args) {
 
 		
-		ProcessComposedMappings moProcess = new ProcessComposedMappings();
+		ProcessComposedMappings moProcess = new ProcessComposedMappings(true); // saveToCSV = true;
+		System.out.println(moProcess.availableStatistics);
 		MediatingOntologiesUtils moUtils = new MediatingOntologiesUtils();
 		moUtils.getParentFolder(args);
 		moUtils.readConfigJSON();
@@ -94,26 +140,42 @@ public class ProcessComposedMappings{
 			FlatAlignmentReader mappingReader = new FlatAlignmentReader(sourceTargetMappingsFile);
 			Set<MappingObjectStr> mapSource2Target = mappingReader.getMappingObjects();
 			System.out.println("Original set of mappings contains " + mapSource2Target.size() + " mappings");
-
+			
+			File file = new File(moUtils.parentPath + "stats.csv");
+			// Create a File and append if it already exists.
+			Writer writer = new FileWriter(file, true);
+			//Reader reader = new FileReader(file);
+			String eol = System.getProperty("line.separator");
+			System.out.println("EOL: " + eol + "is here");
 
 			HashMap<String,FlatAlignmentReader> readersArray = moProcess.makeMappingReadersFromDirectory(moMappingsPath);
 			for( String ontoLabel: readersArray.keySet()) {
-				
+					HashMap<String,String> statsRow = moProcess.createDataRow();
+
 					Set<MappingObjectStr> moComposedMappings = readersArray.get(ontoLabel).getMappingObjects();
 					System.out.println("Mediating ontology gave a total of " + moComposedMappings.size() + " mappings");
 					Set<MappingObjectStr> newMappings = moProcess.mappingSetSubtraction(moComposedMappings, mapSource2Target);
 					System.out.println("Of which new mappings are " + newMappings.size());
 					String newMapPath = newMappingsPath + ontoLabel;
 					moUtils.saveOntologyMappings(newMappings, newMapPath, onto1_iri, onto2_iri);
-
+					
+					statsRow.replace("ontology_label", ontoLabel);
+					statsRow.replace("all_composed_mappings_count", String.valueOf(moComposedMappings.size()));
+					statsRow.replace("unique_mappings_count_minus_logmap", String.valueOf(newMappings.size()));
+					moProcess.addRowToCSV(writer,eol,statsRow);
 				}
+
+			writer.close();
+			//reader.close();
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 }
+
 				
 
-		
 
 	
--- src/test/java/uk/ac/ox/krr/logmap2/mediating_ontologies/README.md
+++ src/test/java/uk/ac/ox/krr/logmap2/mediating_ontologies/README.md
@@ -93,3 +93,7 @@ Where we take the mappings obtained via each mediating ontology and we subtract 
 ```
 java -cp logmap-matcher-4.0-tests.jar:logmap-matcher-4.0.jar 
           uk.ac.ox.krr.logmap2.mediating_ontologies.ProcessComposedMappings <parent folder>
+          
+```
+
+This step creates a new sub-directory `store-unique-mappings` which will contain all the txt/tsv files with the new unique mappings produced via mediating ontologies. These are the result of subtracting the set of Logmap direct mappings between source and target, from the set of all mappings obtained using Logmap via the top (available) 10 mediating ontologies.
